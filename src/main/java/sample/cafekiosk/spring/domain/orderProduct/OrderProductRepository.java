a9d114d4eaa47c09a688c35f4c9bec3c1a8a8eef
mygoaway <mygoaway2@gmail.com>
2023-07-04 23:23:14 +0900

    중복되는 상품번호 리스트로 주문을 가능하게끔 OrderService 로직 변경 및 관련 테스트 수정

---
+++ src/main/java/sample/cafekiosk/spring/domain/orderProduct/OrderProductRepository.java
@@ -0,0 +1,8 @@
+package sample.cafekiosk.spring.domain.orderProduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sample.cafekiosk.spring.domain.order.Order;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+}
--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -2,12 +2,15 @@ package sample.cafekiosk.spring.api.service.order;
 
 import org.assertj.core.api.Assertions;
 import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -24,12 +27,23 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
     @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+    @Autowired
     private ProductRepository productRepository;
 
 
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
+
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
-    void test() {
+    void createOrder() {
         //given
         LocalDateTime registeredTime = LocalDateTime.now();
 
@@ -55,6 +69,38 @@ class OrderServiceTest {
                 .containsExactlyInAnyOrder(
                         Tuple.tuple("001", 1000),
                         Tuple.tuple("002", 3000)
+                );
+
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        //given
+        LocalDateTime registeredTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        // when
+        OrderResponse orderResponse =  orderService.createOrder(request, registeredTime);
+
+        // then
+        Assertions.assertThat(orderResponse.getId()).isNotNull();
+        Assertions.assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredTime, 2000);
+        Assertions.assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("001", 1000)
                 );
 
     }
