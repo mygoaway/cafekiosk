7837cba8aeb1757a2d3bc07f820fae8b23a41b1d
mygoaway <mygoaway2@gmail.com>
2023-07-06 20:15:32 +0900

    OrderController 테스트 반영

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.OrderService;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RestController
@@ -17,9 +20,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public void createOrder(@RequestBody OrderCreateRequest request) {
-        orderService.createOrder(request, LocalDateTime.now());
-
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
+        return ApiResponse.ok(orderService.createOrder(request.toServiceRequest(), LocalDateTime.now()));
     }
 
 }
